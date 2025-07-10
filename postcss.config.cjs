5fdb031e8b0ffea74b3517910bead6f57e6de3ed
Zijie Cai <leo20020208@gmail.com>
2025-07-09 22:13:56 -0400

    gh

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-// postcss.config.cjs
-module.exports = {
-  plugins: {
-    tailwindcss: {},    // <-- use this, not @tailwindcss/postcss
-    autoprefixer: {},
-  },
-};
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/car-location-logger/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',   
+  },
 });
